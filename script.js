9d9469137bb3c00d2c6da44853177f7bf7950510
Ruminov <74424348+Ruminov@users.noreply.github.com>
2024-06-04 23:23:54 -0400

    Complete the CSS calculator layout

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-const wrapper = document.querySelector(".wrapper");
-const display = document.querySelector("p.number");
+const display = document.querySelector("p.display");
+const characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-const characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ","];
-
-let n1;
-let n2;
+let n1 = "";
+let n2 = "";
 
 function writeNum(e) {
-  if (characters.includes(e.key)) n1 ? (n1 += e.key) : (n1 = e.key);
+  if (!characters.includes(e.key)) return;
+  n1 ? (n1 += e.key) : (n1 = e.key);
+}
+
+function writeFloatNum(e) {
+  if ([",", "."].includes(e.key) && !n1.includes(",")) n1 += ",";
   display.textContent = n1;
 }
 
 function deleteNum(e) {
   if (e.key === "Backspace") {
-    n1.textContent = n1.textContent.slice(0, -1);
+    n1 = n1.slice(0, -1);
+  }
+  display.textContent = n1;
+}
+
+function clear(e) {
+  if (e.key === "r") {
+    n1 = "";
+    n2 = "";
+    display.textContent = "0";
   }
 }
 
 function showNum(e) {
-  if (e.key === "Enter") console.log(n1.textContent);
+  if (e.key === "Enter") return;
+  console.log("Boom");
 }
 
 function operate() {}
 
 document.addEventListener("keydown", writeNum);
 document.addEventListener("keydown", deleteNum);
-document.addEventListener("keydown", showNum);
+document.addEventListener("keydown", clear);
